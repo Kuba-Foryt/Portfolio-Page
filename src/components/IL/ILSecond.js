2b4df504fc5b781e277dee471bc1950216e3e1fd
Kuba Foryt <kubfor@gmail.com>
2022-05-11 12:21:14 +0200

    media quaries to IL page

--- src/components/IL/ILSecond.js
+++ src/components/IL/ILSecond.js
@@ -7,14 +7,17 @@ const ILSecond = () => {
     <div className={styles.container}>
       <Slider />
       <div className={styles.text}>
-        Immersive Lighting is culmination of my work on lighting system for
-        Witcher 3. It's prepared as complete overhaul of lighting in base game
-        and two DLCs. <br></br>Project purpose was to deliver enjoyable product,
-        give fantasy vibe at day and realistic vibe during nights, eliminate
-        ugly lights put into cutscenes, extend weather system in all regions. It
-        introduces many things not present in vanilla as volumetric clouds in
-        Skellige, eliminated environmental light probes in all regions and many
-        other things I forgot to mention here.
+        <div>
+          {" "}
+          Immersive Lighting is culmination of my work on lighting system for
+          Witcher 3. It's prepared as complete overhaul of lighting in base game
+          and two DLCs. <br></br>Project purpose was to deliver enjoyable
+          product, give fantasy vibe at day and realistic vibe during nights,
+          eliminate ugly lights put into cutscenes, extend weather system in all
+          regions. It introduces many things not present in vanilla as
+          volumetric clouds in Skellige, eliminated environmental light probes
+          in all regions and many other things I forgot to mention here.
+        </div>
       </div>
     </div>
   );
